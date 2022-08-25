3ac08c56bb7e2b74ca6f924209bad1fb5d365e6f
Fluffy Fox <cosmo8072@gmail.com>
2022-08-25 18:17:13 +0700

    Updates

---
+++ build.cmake
@@ -0,0 +1,43 @@
+# Build config
+
+set(BUILD_PROJECT_NAME "ExampleProject")
+
+# We're making library
+set(BUILD_IS_LIBRARY NO)
+
+# If we want make libary and
+# executable project
+set(BUILD_INSTALL_EXECUTABLE YES)
+
+# Sources which common between exe and library
+set(BUILD_SOURCES
+  src/dummy.c
+)
+
+# Note that exe does not represent Windows' 
+# exe its just short hand of executable 
+#
+# Note:
+# Still creates executable even building library. 
+# This would contain test codes if project is 
+# library. The executable directly links to the 
+# library objects instead through shared library
+set(BUILD_EXE_SOURCES
+  src/specials.c
+  src/premain.c
+  src/main.c
+)
+
+# Public header to be exported
+# If this a library
+set(BUILD_PUBLIC_HEADERS
+  include/dummy.h
+)
+
+# AddPkgConfigLib is in CMakeLists.txt
+macro(AddDependencies)
+  # Example
+  # AddPkgConfigLib(FluffyGC FluffyGC>=1.0.0)
+endmacro()
+
+
